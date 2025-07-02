b2d829e9687b53d278c36b21df7ede5559b2b499
Shakhriyorbek98 <sh.oromjonov@mkb.uz>
2025-07-02 17:51:47 +0500

    Update hw5.sql

--- lesson-5/homework/hw5.sql
+++ lesson-5/homework/hw5.sql
@@ -12,3 +12,19 @@ CREATE TABLE Products (
 select *from Products
 
 select ProductName as Name from Products
+
+
+CREATE TABLE Customers (
+    CustomerID INT PRIMARY KEY,
+    FirstName VARCHAR(100),
+    LastName VARCHAR(100),
+    Email VARCHAR(100),
+    Phone VARCHAR(50),
+    Address VARCHAR(255),
+    City VARCHAR(100),
+    State VARCHAR(100),
+    PostalCode VARCHAR(20),
+    Country VARCHAR(100)
+);
+
+Select *from Customers as Client 
